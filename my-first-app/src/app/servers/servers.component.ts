6cf39b1ce2200fa9c61de016fa111ddaf90b018c
sebgos <gos.sebastian@gmail.com>
2018-11-19 02:16:32 +0100

    string interpolation, property, event and two way data binding example

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
   serverName = 'Test server';
+  username = '';
 
   constructor() {
     setTimeout(() => {
