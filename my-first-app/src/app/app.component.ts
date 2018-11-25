2114436ac81782b40b7328202d2032c25a5c2bed
Sebastian Gos <gos.sebastian@gmail.com>
2018-11-25 09:17:47 +0100

    practicing directives

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color:dodgerblue;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  show = false;
+  log = [];
+  onToggleShowDetails() {
+    this.show = !this.show;
+    this.log.push(this.log.length + 1);
+  }
 }
